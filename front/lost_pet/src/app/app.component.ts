38c6e3490378be5df09f1726d6125cf4e1f34a37
bernabe18 <leanvallejos666@gmail.com>
2024-09-30 12:26:49 -0300

    solucion de reuters

--- front/lost_pet/src/app/app.component.ts
+++ front/lost_pet/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    LoginComponent,
+    RegisterComponent
+],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/lost_pet/src/app/app.routes.ts
+++ front/lost_pet/src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent },
+    {path: 'register', component:RegisterComponent}
+];
+
+
+export default routes;
